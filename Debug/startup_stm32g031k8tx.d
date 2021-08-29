412553e0b7370862ab5add7f76338fbf333d35dd
SafaBulat <safa.bulat2014@gtu.edu.tr>
2021-08-29 16:17:43 +0300

    calc v1.0
    Scientific Calculator project for my microprocessor class fall 2020. It works with 4x4 keypad and 4x seven segment display.
    Features:
    -It computes several operations between -999 , 9999 range.
    -It can show negative and float numbers
    -Calculations that you can operate;
    addition, subtraction, multiplication, divison, log, ln, sqrt, power, sin, cos, tan, cot, pi...

---
+++ Debug/startup_stm32g031k8tx.d
@@ -0,0 +1,2 @@
+startup_stm32g031k8tx.o: \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/startup_stm32g031k8tx.s
---
+++ Debug/system_stm32g0xx.d
@@ -0,0 +1,26 @@
+system_stm32g0xx.o: \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/system_stm32g0xx.c \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/stm32g0xx.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/stm32g031xx.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/core_cm0plus.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/cmsis_version.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/cmsis_compiler.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/cmsis_gcc.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/mpu_armv7.h \
+ C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/system_stm32g0xx.h
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/stm32g0xx.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/stm32g031xx.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/core_cm0plus.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/cmsis_version.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/cmsis_compiler.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/cmsis_gcc.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/mpu_armv7.h:
+
+C:/Users/safa/Desktop/PCWorkStuff/Embedded/stm32g0/include/system_stm32g0xx.h:
